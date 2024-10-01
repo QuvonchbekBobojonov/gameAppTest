95e909adac37c0e216cc704e80e5be2afd07851c
Quvonchbek Bobojonov <hi@moorfo.uz>
2024-10-01 09:21:56 +0500

    test

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
-import ctypes
 import sys
-import os
+import subprocess
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 
 
-def is_admin():
-    """Check if the script is running with admin privileges."""
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+class CustomExplorer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        # Full screen mode
+        self.showFullScreen()
+
+    def initUI(self):
+        self.setWindowTitle('Custom Explorer')
+        self.setGeometry(100, 100, 800, 600)
+
+        layout = QVBoxLayout()
+
+        # Add buttons for different functionalities
+        open_file_explorer_button = QPushButton('Open File Explorer', self)
+        open_file_explorer_button.clicked.connect(self.open_file_explorer)
+
+        shutdown_button = QPushButton('Shutdown System', self)
+        shutdown_button.clicked.connect(self.shutdown_system)
+
+        restart_button = QPushButton('Restart System', self)
+        restart_button.clicked.connect(self.restart_system)
+
+        layout.addWidget(QLabel("Welcome to Custom Explorer", self))
+        layout.addWidget(open_file_explorer_button)
+        layout.addWidget(shutdown_button)
+        layout.addWidget(restart_button)
+
+        self.setLayout(layout)
+
+    def open_file_explorer(self):
+        # Open the default Windows file explorer
+        subprocess.Popen('explorer')
+
+    def shutdown_system(self):
+        # Shutdown the system
+        subprocess.Popen('shutdown /s /t 0')
+
+    def restart_system(self):
+        # Restart the system
+        subprocess.Popen('shutdown /r /t 0')
 
 
-if __name__ == "__main__":
-    if is_admin():
-        # Put the code that requires admin privileges here
-        print("You are running with administrator privileges.")
-        # Example: Accessing protected system directories, modifying registry, etc.
-    else:
-        # If not running as admin, request elevation
-        print("Requesting administrator privileges...")
-        # Restart script with admin rights
-        ctypes.windll.shell32.ShellExecuteW(
-            None, "runas", sys.executable, os.path.abspath(__file__), None, 1
-        )
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = CustomExplorer()
+    window.show()
+    sys.exit(app.exec())
