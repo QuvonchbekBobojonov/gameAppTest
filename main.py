1022863dcdbbfcaa51603c201fb3157532a6d9a0
Quvonchbek Bobojonov <hi@moorfo.uz>
2024-10-01 09:42:09 +0500

    test

--- main.py
+++ main.py
@@ -45,6 +45,17 @@ class CustomExplorer(QWidget):
         # Restart the system
         subprocess.Popen('shutdown /r /t 0')
 
+    def closeEvent(self, event):
+        # Override close event to disable Alt+F4 or window close
+        reply = QMessageBox.question(self, 'Close Confirmation',
+                                     "Closing is disabled. Would you like to exit?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()  # Allow close if "Yes" is clicked
+        else:
+            event.ignore()  # Ignore the close event
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
