68f8f43a5664af215c345e21a6d9706cdc34199c
Quvonchbek Bobojonov <hi@moorfo.uz>
2024-10-02 10:32:29 +0500

    test

--- client.py
+++ client.py
@@ -1,68 +1,61 @@
 import socket
 import subprocess
-import os
-import glob
+import winreg
+
+# Constants for commands
+COMMAND_EXIT = 'exit'
+COMMAND_APPS = 'apps'
+COMMAND_OPEN = 'open:'
+COMMAND_CLOSE = 'close:'
+COMMAND_SHUTDOWN = 'shutdown'
+COMMAND_RESTART = 'restart'
 
 
-def get_start_menu_links():
-    """Fetches all Start Menu shortcut (.lnk) files from both user and system Start Menu directories."""
-    user_start_menu = os.path.expandvars(r'%APPDATA%\Microsoft\Windows\Start Menu\Programs')
-    system_start_menu = os.path.expandvars(r'%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs')
+def get_installed_apps():
+    apps = []
+    uninstall_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
 
-    # Recursively search for all .lnk files in the Start Menu directories
-    user_links = glob.glob(os.path.join(user_start_menu, '**', '*.lnk'), recursive=True)
-    system_links = glob.glob(os.path.join(system_start_menu, '**', '*.lnk'), recursive=True)
+    try:
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, uninstall_key) as key:
+            for i in range(winreg.QueryInfoKey(key)[0]):
+                try:
+                    app_name = winreg.QueryValueEx(winreg.OpenKey(key, winreg.EnumKey(key, i)), 'DisplayName')[0]
+                    apps.append(app_name)
+                except FileNotFoundError:
+                    continue
+    except FileNotFoundError:
+        print("Could not access the uninstall registry key.")
 
-    return user_links + system_links
+    return apps
 
 
 def handle_client_commands(s, apps):
-    """Handles client commands received from the server and processes them."""
     while True:
         try:
-            # Receive data from the server
             data = s.recv(1024).decode()
             if not data:
-                break  # If no data is received, exit the loop
+                break
 
-            if data == 'exit':
+            if data == COMMAND_EXIT:
                 s.send("Exiting...".encode())
                 print("Exiting...")
                 break
 
-            elif data == 'apps':
-                # Send the list of available apps (start menu shortcuts)
-                for i, app in enumerate(apps):
-                    s.send(f"{i}: {app}\n".encode())
+            elif data == COMMAND_APPS:
+                apps_list = '\n'.join(f"{i}: {app}" for i, app in enumerate(apps))
+                s.send(apps_list.encode())
 
-            elif data.startswith('open:'):
-                try:
-                    index = int(data.split(':')[1])
-                    if 0 <= index < len(apps):
-                        # Open the application at the specified index
-                        subprocess.run(['explorer', apps[index]], shell=True)
-                        s.send(f"Opened {apps[index]}\n".encode())
-                    else:
-                        s.send(f"Invalid app index: {index}\n".encode())
-                except (ValueError, IndexError):
-                    s.send("Invalid command format or index.\n".encode())
-            elif data.startswith('close:'):
-                try:
-                    index = int(data.split(':')[1])
-                    if 0 <= index < len(apps):
-                        # Close the application at the specified index
-                        subprocess.run(['taskkill', '/f', '/im', apps[index]], shell=True)
-                        s.send(f"Closed {apps[index]}\n".encode())
-                    else:
-                        s.send(f"Invalid app index: {index}\n".encode())
-                except (ValueError, IndexError):
-                    s.send("Invalid command format or index.\n".encode())
+            elif data.startswith(COMMAND_OPEN):
+                handle_application(s, data, apps, 'open')
 
-            elif data == 'shutdown':
+            elif data.startswith(COMMAND_CLOSE):
+                handle_application(s, data, apps, 'close')
+
+            elif data == COMMAND_SHUTDOWN:
                 s.send("Shutting down the machine...\n".encode())
                 subprocess.run('shutdown /s /t 0', shell=True)
 
-            elif data == 'restart':
+            elif data == COMMAND_RESTART:
                 s.send("Restarting the machine...\n".encode())
                 subprocess.run('shutdown /r /t 0', shell=True)
 
@@ -74,29 +67,41 @@ def handle_client_commands(s, apps):
             break
 
 
+def handle_application(s, data, apps, action):
+    try:
+        index = int(data.split(':')[1])
+        if 0 <= index < len(apps):
+            app_name = apps[index]
+            if action == 'open':
+                subprocess.run(['explorer', app_name], shell=True)
+                s.send(f"Opened {app_name}\n".encode())
+            elif action == 'close':
+                subprocess.run(['taskkill', '/f', '/im', f"{app_name}.exe"], shell=True)
+                s.send(f"Closed {app_name}\n".encode())
+        else:
+            s.send(f"Invalid app index: {index}\n".encode())
+    except (ValueError, IndexError):
+        s.send("Invalid command format or index.\n".encode())
+
+
 def main():
-    """Main function to establish a connection to the server and handle client commands."""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket successfully created")
 
     host = '192.168.0.133'
     port = 6666
 
-    # Get the list of Start Menu apps
-    apps = get_start_menu_links()
+    apps = get_installed_apps()
 
     try:
-        # Connect to the server
         s.connect((host, port))
         print(f"Connected to server {host} on port {port}")
     except socket.error as e:
         print(f"Connection error: {e}")
         return
 
-    # Handle client commands
     handle_client_commands(s, apps)
 
-    # Close the connection
     s.close()
 
 
